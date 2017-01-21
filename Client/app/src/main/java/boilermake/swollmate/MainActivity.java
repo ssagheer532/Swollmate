f3e595342b31f888f2dea080c73620178dc201f3
Ahad <ssagheer@purdue.edu>
2017-01-21 00:47:24 -0500

    added discover gui

--- Client/app/src/main/java/boilermake/swollmate/MainActivity.java
+++ Client/app/src/main/java/boilermake/swollmate/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent intent = new Intent(MainActivity.this, UserProfileActivity.class);
+        Intent intent = new Intent(MainActivity.this, DiscoverActivity.class);
         startActivity(intent);
     }
 }
