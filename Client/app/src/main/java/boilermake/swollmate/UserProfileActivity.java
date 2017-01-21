101fc4eaa9a1d073893fe72cd0248fc0c71cef6d
Farhan Shafi <fshafi@purdue.edu>
2017-01-21 00:27:06 -0500

    added user profile page gui

--- Client/app/src/main/java/boilermake/swollmate/UserProfileActivity.java
+++ Client/app/src/main/java/boilermake/swollmate/UserProfileActivity.java
@@ -2,12 +2,30 @@ package boilermake.swollmate;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
 
-public class UserProfileActivity extends AppCompatActivity {
+public class UserProfileActivity extends AppCompatActivity  {
+
+    CheckBox gainMuscleGoal, leisureGoal, looseWeightGoal, sportsGoal;
+    CheckBox beginnerLevel, expertLevel, intermediateLevel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
+
+        gainMuscleGoal = (CheckBox) findViewById(R.id.Gain_Muscle);
+        leisureGoal = (CheckBox) findViewById(R.id.Leisure);
+        looseWeightGoal = (CheckBox) findViewById(R.id.Loose_Weight);
+        sportsGoal = (CheckBox) findViewById(R.id.Sports);
+
+        beginnerLevel = (CheckBox) findViewById(R.id.Beginner);
+        expertLevel = (CheckBox) findViewById(R.id.Expert);
+        intermediateLevel = (CheckBox) findViewById(R.id.Intermediate);
+
+
     }
+
+
 }
