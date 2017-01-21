acd1eb1c9ff0a61784de17ddbcfe733ad97d8985
Farhan Shafi <fshafi@purdue.edu>
2017-01-21 01:32:44 -0500

    Added button group and other for UserProfile Activity

--- Client/app/src/main/java/boilermake/swollmate/UserProfileActivity.java
+++ Client/app/src/main/java/boilermake/swollmate/UserProfileActivity.java
@@ -4,14 +4,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
-public class UserProfileActivity extends AppCompatActivity  {
+public class UserProfileActivity extends AppCompatActivity {
 
     CheckBox gainMuscleGoal, leisureGoal, looseWeightGoal, sportsGoal;
     CheckBox beginnerLevel, expertLevel, intermediateLevel;
+    private RadioGroup radioSexGroup;
+    private RadioButton radioSexButton;
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    /*protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
 
@@ -25,7 +30,16 @@ public class UserProfileActivity extends AppCompatActivity  {
         intermediateLevel = (CheckBox) findViewById(R.id.Intermediate);
 
 
+    }*/
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_profile);
+        radioSexGroup = (RadioGroup) findViewById(R.id.radioGroup);
+
+        int selectedId = radioSexGroup.getCheckedRadioButtonId();
+        radioSexButton = (RadioButton) findViewById(selectedId);
+
+
     }
-
-
 }
