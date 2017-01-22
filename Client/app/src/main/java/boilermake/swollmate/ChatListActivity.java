711961acd3bac54116d8124b07c4bfb19bc52a2c
Ahad <ssagheer@purdue.edu>
2017-01-22 07:20:48 -0500

    chat code

--- Client/app/src/main/java/boilermake/swollmate/ChatListActivity.java
+++ Client/app/src/main/java/boilermake/swollmate/ChatListActivity.java
@@ -1,18 +1,24 @@
 package boilermake.swollmate;
 
-import android.graphics.Color;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.LinearLayout;
 
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 import static boilermake.swollmate.MainActivity.buttons;
+import static boilermake.swollmate.MainActivity.me;
 
 public class ChatListActivity extends AppCompatActivity {
+
+    private DatabaseReference matchedDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,10 +39,58 @@ public class ChatListActivity extends AppCompatActivity {
         b1.setTextSize(18);
         ll.addView(b1);
 
-        
+      //  addButton("");
+
     }
 
-    public void addButton() {
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        matchedDatabase = FirebaseDatabase.getInstance().getReference().child("matched");
+
+
+        ChildEventListener childListener = new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                String value = (String) dataSnapshot.getValue().toString();
+               // Log.d("test1: ", me.uID);
+                if (value.contains(me.uID)) {
+                    value = value.replace(me.uID, "");
+                    value = value.trim();
+                    addButton(value);
+                    Log.d("test2: ", value);
+
+                }
+
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        };
+        matchedDatabase.addChildEventListener(childListener);
+
+    }
+
+    public void addButton(String name) {
+
         LinearLayout ll = (LinearLayout) findViewById(R.id.linear);
         LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
                 Toolbar.LayoutParams.MATCH_PARENT, 300);
@@ -46,8 +100,11 @@ public class ChatListActivity extends AppCompatActivity {
         buttons.add(b1);
         b1.setLayoutParams(params);
         b1.setPadding(150, 0, 0, 0);
-        b1.setText("Button");
-        b1.setTextSize(18);
+        b1.setText(name);
+        b1.setTextSize(14);
         ll.addView(b1);
     }
+
+
+
 }
